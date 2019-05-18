29265e8a084831e82805d20a19e2abbebe62cc09
qw <ikherty@gmail.com>
2019-05-19 00:19:25 +0300

    begin understand

--- deque.cpp
+++ deque.cpp
@@ -1,6 +1,69 @@
 #include "deque.h"
 
-Deque::Deque()
+template <typename Type>
+void Deque<Type>::Creation(Deque *D){
+    D->last=0;
+}
+template <typename Type>
+bool Deque<Type>::Full(Deque *D){
+if (D->last==0) return true;
+else return false;
+}
+template <typename Type>
+void Deque<Type>::AddL(Deque *D)
 {
+if (D->last==sizeDeq){
+    cout<<"\nДек заполнен\n\n";
+    return;
+}
+int value;
+cout<<"\nЗначение > ";
+cin>>value;
+for (int i=D->last; i>0; i--)
+    D->data[i]=D->data[i-1];
+D->data[0]=value;
+D->last++;
+cout<<endl<<"Элемент добавлен\n\n";
+}
 
+template <typename Type>
+void Deque<Type>::AddR(Deque *D)
+{
+if (D->last==sizeDeq){
+    cout<<"\nДек заполнен\n\n";
+    return;
+}
+int value;
+cout<<"\nЗначение > ";
+cin>>value;
+D->data[D->last++]=value;
+cout<<endl<<"Элемент добавлен\n\n";
+}
+
+template <typename Type>
+void Deque<Type>::DeleteL(Deque *D)
+{
+for (int i=0; i<D->last; i++)
+    D->data[i]=D->data[i+1];
+D->last--;
+}
+
+template <typename Type>
+void Deque<Type>::DeleteR(Deque *D){
+    D->last--;
+}
+
+template <typename Type>
+int Deque<Type>::OutputL(Deque *D){
+    return D->data[0];
+}
+
+template <typename Type>
+int Deque<Type>::OutputR(Deque *D){
+    return D->data[D->last-1];
+}
+
+template <typename Type>
+int Deque<Type>::Size(Deque *D){
+    return D->last;
 }
--- deque.h
+++ deque.h
@@ -1,11 +1,26 @@
 #ifndef DEQUE_H
 #define DEQUE_H
 #include <deque>
-
+#include <vector>
+#include <iostream>
+using namespace std;
+//шаблонный класс на основе серверного
+template <typename Type>
 class Deque
 {
+private:
+    const int sizeDeq; //размер дека
+    Type last; //указатель на конец
+    vector <Type> v1;//очередь-объект серверного класса
 public:
-    Deque();
+    void Creation(Deque *D); //создание дека
+    bool Full(Deque *D); //проверка дека на пустоту
+    void AddL(Deque *D); //добавление элемента в начало
+    void AddR(Deque *D); //добавление элемента в конец
+    void DeleteL(Deque *D); //удаление первого элемента
+    void DeleteR(Deque *D); //удаление последнего элемента
+    int OutputL(Deque *D); //вывод первого элемента
+    int OutputR(Deque *D); //вывод последнего элемента
+    int Size(Deque *D); //размер дека
 };
-
 #endif // DEQUE_H
